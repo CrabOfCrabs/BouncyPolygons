9d4feabef8797cce8da1ceb8a3cf9d627737ec5f
CrabOfCrabs <95616091+CrabOfCrabs@users.noreply.github.com>
2022-03-08 23:52:18 +0100

    math hard
    grrr

--- core.c
+++ core.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <sys/ioctl.h>
 #include <time.h>
+#include <math.h>
 struct winsize w;
 #define MAX_ARR 128
 
@@ -68,7 +69,7 @@ void Add_PointToObj(Vec2 p,Obj o){
 void DSline(double x1,double x2,double y ){ //Draws Simple line
 	double x,x2l;
 	if(x1<=x2){x=x1;x2l = x2;}else{x=x2;x2l = x1;}
-	do{mvprintw(y/2,x,"#");}while(x++<x2l);}
+	do{mvprintw(y,x,"#");}while(x++<x2l);}
 
 void drawtup(Tri t){
 	Vec2 *p1 = t.p1,*p2 = t.p2,*p3 = t.p3;
@@ -106,40 +107,75 @@ void move_Tri(Tri* tm,Vec2 aF){
 
 
 
-void border_Check(Vec2 screen ,Vec2 *p,Vec2 *f,Tri *tp){
-		if(p->y > screen.y){Vec2 po = {0,-1};move_Tri(tp,po);f->y*=-1;}
-		else if(p->y < 1){Vec2 po = {0,1};move_Tri(tp,po);f->y*=-1;}
-		if(p->x >  screen.x){Vec2 po = {0,-1};move_Tri(tp,po);f->x*=-1;}
-		else if(p->x < 1){Vec2 po = {1,0};move_Tri(tp,po);f->x*=-1;}}
+double border_Check(Vec2 screen ,Vec2 *p,Vec2 *f,Tri *tp,Vec2 cen){
+		if(p->y > screen.y){Vec2 po = {0,screen.y - p->y};	//workspace roof
+
+			move_Tri(tp,po);f->y*=-1;
+		Vec2 U = {f->x,f->y};Vec2 V = {p->x - cen.x , p->y - cen.y};return ((U.x*V.y)-(U.y*V.x));
+		}			
+		else if(p->y < 0){Vec2 po = {0,-p->y};			//workspace bottom
+
+			move_Tri(tp,po);f->y*=-1;
+		Vec2 U = {f->x,f->y};Vec2 V = {p->x - cen.x , p->y - cen.y};return ((U.x*V.y)-(U.y*V.x));		
+		}
+		if(p->x >  screen.x){Vec2 po = {screen.x - p->x,0};	//workspace left
+									
+			move_Tri(tp,po);f->x*=-1;
+		Vec2 U = {f->x,f->y};Vec2 V = {p->x - cen.x , p->y - cen.y};return ((U.x*V.y)-(U.y*V.x));
+		}
+		else if(p->x < 0){Vec2 po = {-p->x,0};			//workspace right
+
+			move_Tri(tp,po);f->x*=-1;
+		Vec2 U = {f->x,f->y};Vec2 V = {p->x - cen.x , p->y - cen.y};return ((U.x*V.y)-(U.y*V.x));
+		}
+		else{return 0;}
+		
 
 
+}
 
+
+void rotateVec(Vec2 *p,Vec2 cen,double torque){
+	Vec2 pR = {((p->x-cen.x)*cos(torque)-(p->y-cen.y)*sin(torque))+cen.x , ((p->y-cen.y)*cos(torque)+(p->x-cen.x)*sin(torque))+cen.y};
+	
+	p->x = pR.x;p->y = pR.y;
+}
+
+void rotateTri(Tri *tp,Vec2 cen,double torque){
+	
+	rotateVec(tp->p1,cen,torque);
+	rotateVec(tp->p2,cen,torque);
+	rotateVec(tp->p3,cen,torque);
+}
 
 
 Vec2 tricenp(Tri t){
 	Vec2 p = {(t.p1->x+t.p2->x+t.p3->x)/3,(t.p1->y+t.p2->y+t.p3->y)/3};
 	return p;}
-int main(){int ms = 9;
+int main(){int ms = 20;
 ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
 	time_t start, now;struct timespec delay;delay.tv_sec = 0;delay.tv_nsec = ms * 999999L;time(&start);
 
 	initscr();
         clear();
-	Vec2 pt1 = {10,20},pt2 = {20,10},pt3 = {20,25};
+	Vec2 pt1 = {25,20},pt2 = {20,10},pt3 = {30,20};
 	Tri tp = {&pt1,&pt2,&pt3};
-	Vec2 force = {1,0};
+	Vec2 force = {0.1,0};
+	double torque = 0;
 	int gravacc = 0;
 	while(1){
 		ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
 		Vec2 screen = {w.ws_col,w.ws_row};
 		//gravacc =+ 0.1;
-		force.y =force.y+ 0.1;
-		border_Check(screen,tp.p1,&force,&tp);					
-		border_Check(screen,tp.p2,&force,&tp);	
-		border_Check(screen,tp.p3,&force,&tp);
+		Vec2 cen = tricenp(tp);rotateTri(&tp,cen,torque/180);
 
+		force.y =force.y+ 0.01;move_Tri(&tp,force);
+
+		torque = torque + border_Check(screen,tp.p1,&force,&tp,cen);					
+		torque = torque + border_Check(screen,tp.p2,&force,&tp,cen);	
+		torque = torque + border_Check(screen,tp.p3,&force,&tp,cen);
 		nanosleep(&delay,NULL);
-		clear();move_Tri(&tp,force);
+		clear();
 		scanln(tp);
 		refresh();}
 
