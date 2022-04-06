3c6c4576dca6d1427325a5d743edabaf11793c44
CrabOfCrabs <madcrabgames@gmail.com>
2022-04-06 19:33:46 +0200

    modified:   main.c

--- main.c
+++ main.c
@@ -21,15 +21,21 @@ typedef struct triangle{
 }Tri;
 
 void moveVec(Vec2 *v,Vec2 offs){
-	v->x += offs.x;
-	v->y += offs.y;}
+	v->x += offs.x/100;
+	v->y += offs.y/100;}
 void moveTri(Tri *t,Vec2 offs){
 	moveVec(&t->p1,offs);
 	moveVec(&t->p2,offs);
 	moveVec(&t->p3,offs);}
+Vec2 addVec(Vec2 p,Vec2 offs){
+	Vec2 v;
+	v.x =p.x+ offs.x;
+	v.y =p.y+ offs.y;
+	return v;
+}
 
 double dot(Vec2 p1 , Vec2 p2){
-	double prod = p1.x * p2.x + p1.y + p2.y;
+	double prod = p1.x * p2.x + p1.y * p2.y;
 	return prod;}
 
 Vec2 cenTri(Tri t){
@@ -72,8 +78,8 @@ void drawtdown(Tri t){
                 DSline(curx1,curx2,Y);
                 curx1 -= invslope1;curx2 -= invslope2;}}
 
-void scanln(Tri tr){
-        //tri tr = projt(rent(t,xr),screen);
+void scanln(Tri t){
+        Tri tr = t;
 
         if(tr.p2.y < tr.p1.y){ swapp(&tr.p2, &tr.p1); }if(tr.p3.y <tr.p1.y){ swapp(&tr.p3, &tr.p1); }if(tr.p3.y < tr.p2.y){ swapp(&tr.p3, &tr.p2); }
 
@@ -82,59 +88,81 @@ void scanln(Tri tr){
         else{Vec2 p4 = {inpolx(tr.p1,tr.p3,tr.p2.y),tr.p2.y};
         Tri tu = {tr.p1,tr.p2,p4};Tri td = {tr.p2,p4,tr.p3};
         drawtup(tu);drawtdown(td);}}
+double cross(Vec2 p1,Vec2 p2){return ((p1.x*p2.y)-(p1.y*p2.x));}
 
-double border_Check(Vec2 screen ,Vec2 *R,Vec2 *V,Tri *tp){
-                if(p->y > screen.y){Vec2 po = {0,screen.y - p->y};      //workspace roof
-                        Vec2 U = {f->x,f->y};
-                        Vec2 V = {p->x - cen.x , p->y - cen.y};
-                        move_Tri(tp,po);f->y*=-1;
-                        return ((U.x*V.y)-(U.y*V.x));}
-                else if(p->y < 0){Vec2 po = {0,-p->y};  //workspace bottom
-                        Vec2 U = {f->x,f->y};
-                        Vec2 V = {p->x - cen.x , p->y - cen.y};
-                        move_Tri(tp,po);f->y*=-1;
-                        return ((U.x*V.y)-(U.y*V.x));}
+Vec2 mkvec(double x,double y){Vec2 v = {x,y};return v;}
 
-		else if(p->x >  screen.x){Vec2 po = {screen.x - p->x,0};     //workspace left
-                        Vec2 U = {f->x,f->y};
-                        Vec2 V = {p->x - cen.x , p->y - cen.y};
-                        move_Tri(tp,po);f->x*=-1;
-                        return ((U.x*V.y)-(U.y*V.x));}
-                else if(p->x < 0){Vec2 po = {-p->x,0};  //workspace right
-                        Vec2 U = {f->x,f->y};
-                        Vec2 V = {p->x - cen.x , p->y - cen.y};
-                        move_Tri(tp,po);f->x*=-1;
-                        return ((U.x*V.y)-(U.y*V.x));}
-                else{return 0;}
+Vec2 border_Check(Vec2 screen ,Vec2 p,Vec2 v,Vec2 cen,double w,double I,double mass,Tri *t){
+		Vec2 r = {p.x-cen.x,p.y-cen.y};
+		double j;
+		Vec2 J={0,0};
+                if(p.y > screen.y){
+			moveTri(t,mkvec(0,screen.y-p.y));
+			Vec2 po = {0,-1};      //workspace roof
+                	j = (-2*(dot(v,po)))/(1/mass+(cross(r,po)*cross(r,po)));
+                        Vec2 J1 = {0,-1*j};return J1;}
+		if(p.y < 0){  //workspace bottom
+			moveTri(t,mkvec(0,-p.y));
+			Vec2 po = {0,1};      //workspace roof
+                	j = (-2*(dot(v,po)))/(1/mass+(cross(r,po)*cross(r,po)));
+                        Vec2 J1 = {0,1*j};return J1;}
+	/*	else if(p.x >  screen.x){
+
+			Vec2 po = {1,0};      //workspace roof
+                	j = (-2*(dot(v,po)+w*cross(r,po)))/(1/mass+(cross(r,po)*cross(r,po)));
+                        J.x = 1*j;return J;}
+			
+		else if(p.x < 0){
+			Vec2 po = {1,0};      //workspace roof
+                	j = (-2*(dot(v,po)))/(1/mass+(cross(r,po)*cross(r,po)));
+                        J.x = 1*j;return J;}*/
+		return J;
 
 
 
 }
 
 int main(){
-	int ms = 9;
+	int ms = 10;
 	ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
 	time_t start, now;struct timespec delay;delay.tv_sec = 0;delay.tv_nsec = ms * 999999L;time(&start);
 	
 	initscr();
 	clear();
-	Vec2 p[3] = {{1,2},{2,3},{4,2}};Tri T_test = {p[0],p[1],p[2]};
+	Vec2 p[3] = {{12,33},{6,39},{12,42}};Tri T_test = {p[0],p[1],p[2]};
 
-	double mass = 1;
+
+	double mass = 0.1;
 	
 	double MMOI = mmoiTri(T_test,mass);
 
-	Vec2 linear_V = {0,0};
+	Vec2 v = {0,-10};
 	double angular_V = 0;
 
 
 
 	while(1){
-
+		ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+                Vec2 screen = {w.ws_col,w.ws_row};
+	
 	Vec2 cen = cenTri(T_test);
 
+		Vec2 j3 = border_Check(screen ,T_test.p1,v,cen,angular_V,MMOI,mass,&T_test);
+		Vec2 aj1 ={1/mass*j3.x,1/mass*j3.y};
+		v = addVec(v,aj1);
 
-	}
+		Vec2 j2 =border_Check(screen ,T_test.p2,v,cen,angular_V,MMOI,mass,&T_test);
+		Vec2 aj2 ={1/mass*j2.x,1/mass*j2.y};
+		v = addVec(v,aj2);
 
+		Vec2 j1 =border_Check(screen ,T_test.p3,v,cen,angular_V,MMOI,mass,&T_test);
+		Vec2 aj3 ={1/mass*j1.x,1/mass*j1.y};
+		v = addVec(v,aj3);
+moveTri(&T_test,v);
+		nanosleep(&delay,NULL);
+                clear();
+                scanln(T_test);
+                refresh();}
+	getch();
 	return 0;}
 
